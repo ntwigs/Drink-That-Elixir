83852550d189c13b72a7159356404dc0c5374c46
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-20 22:17:20 +0200

    Moved presentation to Max module

--- statistics/lib/max.ex
+++ statistics/lib/max.ex
@@ -6,8 +6,10 @@ defmodule Max do
   def compare([current|remainder]) do
     next = List.first(remainder)
     new_list = if ( current > next ), do: List.replace_at(remainder, 0, current), else: remainder
-    if (length(remainder) === 1), do: current, else: compare(new_list)
+    if (length(remainder) === 1), do: write(current), else: compare(new_list)
   end
 
   def compare([]), do: nil
+
+  def write(value), do: IO.puts "Max Value: #{ value }"
 end
