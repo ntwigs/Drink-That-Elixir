bb25844b8c87e7ac7b9c1d30314609365f62c294
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-20 22:13:40 +0200

    Gets max value

--- statistics/lib/max.ex
+++ statistics/lib/max.ex
@@ -3,9 +3,10 @@ defmodule Max do
     compare(numbers)
   end
 
-  def compare([first|remainder]) do
-    IO.puts first
-    compare(remainder)
+  def compare([current|remainder]) do
+    next = List.first(remainder)
+    new_list = if ( current > next ), do: List.replace_at(remainder, 0, current), else: remainder
+    if (length(remainder) === 1), do: current, else: compare(new_list)
   end
 
   def compare([]), do: nil
