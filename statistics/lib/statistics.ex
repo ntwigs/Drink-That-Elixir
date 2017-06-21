d8b2e2cbf16400f69ba62e1261912261b3ca58ee
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-21 13:04:14 +0200

    Added mean to statistics

--- statistics/lib/statistics.ex
+++ statistics/lib/statistics.ex
@@ -1,10 +1,12 @@
 defmodule Statistics do
   import Max
   import Min
+  import Mean
 
   def initialize do
     numbers = [5,3,-42,2,4,1337,1442,6,2,1,3,7,100,122]
     Max.get_max(numbers)
     Min.get_min(numbers)
+    Mean.get_mean(numbers)
   end
 end
