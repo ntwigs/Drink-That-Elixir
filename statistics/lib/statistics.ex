c8cd254a82be8493c4351ff4f153dc7ff5102f24
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-21 16:23:07 +0200

    Statistics handles calls

--- statistics/lib/statistics.ex
+++ statistics/lib/statistics.ex
@@ -1,12 +1,30 @@
 defmodule Statistics do
-  import Max
-  import Min
-  import Mean
+  @numberList [5, 3, -42, 2, 4, 1337, 1442, 6, 2, 1, 3, 7, 100, 122] 
 
   def initialize do
-    numbers = [5,3,-42,2,4,1337,1442,6,2,1,3,7,100,122]
-    Max.get_max(numbers)
-    Min.get_min(numbers)
-    Mean.get_mean(numbers)
+    get_max()
+    get_min()
+    get_mean()
+    get_range()
+  end
+
+  def get_max do
+    max_value = Max.get(@numberList)     
+    Presentation.display("Max", max_value)
+  end
+
+  def get_min do
+    min_value = Min.get(@numberList)
+    Presentation.display("Min", min_value)
+  end
+
+  def get_mean do
+    mean_value = Mean.get(@numberList)
+    Presentation.display("Mean", mean_value)
+  end
+
+  def get_range do
+    range_value = Range.get(@numberList)
+    Presentation.display("Range", range_value)
   end
 end
