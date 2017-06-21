4c26986e0a0fe59d8175e6b87f79583e94625036
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-21 12:33:08 +0200

    Using the Min module in statistics

--- statistics/lib/statistics.ex
+++ statistics/lib/statistics.ex
@@ -1,8 +1,10 @@
 defmodule Statistics do
   import Max
+  import Min
 
   def initialize do
-    numbers = [5,3,2,4,1337,6,2,1,3,7,100,122]
+    numbers = [5,3,-42,2,4,1337,1442,6,2,1,3,7,100,122]
     Max.get_max(numbers)
+    Min.get_min(numbers)
   end
 end
