03319244ab9b296b6f35476fce2031d989786c20
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-22 11:38:50 +0200

    Changed remainder length

--- statistics/lib/controller/max.ex
+++ statistics/lib/controller/max.ex
@@ -4,6 +4,6 @@ defmodule Max do
   def compare([current|remainder]) do
     next = List.first(remainder)
     new_list = if ( current > next ), do: List.replace_at(remainder, 0, current), else: remainder
-    if (length(remainder) === 1), do: current, else: compare(new_list)
+    if (length(remainder) === 0), do: current, else: compare(new_list)
   end
 end
