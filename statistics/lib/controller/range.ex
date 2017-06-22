ba2c2875df2d5bf6511956ee9a7a1454f67fc268
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-22 11:39:16 +0200

    Range fix for negative numbers

--- statistics/lib/controller/range.ex
+++ statistics/lib/controller/range.ex
@@ -1,7 +1,7 @@
 defmodule Range do
   def get(numbers) do
     lowest_value = Min.get(numbers)
-    positive_lowest_value = abs(lowest_value)
+    positive_lowest_value = -lowest_value
     higest_value = Max.get(numbers)
     higest_value + positive_lowest_value
   end
