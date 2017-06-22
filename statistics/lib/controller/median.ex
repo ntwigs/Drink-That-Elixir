9364ce7f3935f0e7802f709a7f3551ad1f170bf9
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-22 11:39:04 +0200

    Median works for odd and even numbers

--- statistics/lib/controller/median.ex
+++ statistics/lib/controller/median.ex
@@ -1,3 +1,19 @@
 defmodule Median do
-    
+  def get(numbers) do
+    sorted_list = Enum.sort(numbers)
+    is_even = rem(length(numbers), 2) === 0
+    if (is_even), do: get_center_even(sorted_list), else: get_center_odd(sorted_list) 
+  end
+
+  def get_center_even(sorted_list) do
+    list_center = length(sorted_list) / 2 |> round
+    upper_item = Enum.at(sorted_list, list_center)
+    lower_item = Enum.at(sorted_list, list_center - 1)
+    (lower_item + upper_item) / 2
+  end
+
+  def get_center_odd(sorted_list) do
+    list_center = length(sorted_list) / 2 |> round
+    Enum.at(sorted_list, list_center - 1)
+  end
 end
