e1a1e6538f574ec9337fc5e645b7cf842a428b37
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-22 22:34:58 +0200

    Removed parenthesis from min and added docs

--- statistics/lib/controller/min.ex
+++ statistics/lib/controller/min.ex
@@ -1,11 +1,15 @@
 defmodule Min do
+  @moduledoc """
+    This module gets the lowest value from a list of integers.
+  """
+  
   def get(numbers) do
     compare(numbers)
   end
 
   def compare([current|remainder]) do
     next = List.first(remainder)
-    new_list = if ( current < next ), do: List.replace_at(remainder, 0, current), else: remainder
+    new_list = if current < next, do: List.replace_at(remainder, 0, current), else: remainder
     if (length(remainder) === 0), do: current, else: compare(new_list)
   end
 end
