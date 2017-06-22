d1ceb78092d8bcaf873529a2073e48498878c39f
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-22 22:33:40 +0200

    Removed parenthesis from max and added module doc

--- statistics/lib/controller/max.ex
+++ statistics/lib/controller/max.ex
@@ -1,9 +1,13 @@
 defmodule Max do
+  @moduledoc """
+    This module gets the highest value from a list of integers.
+  """
+
   def get(numbers), do: compare(numbers) 
 
   def compare([current|remainder]) do
     next = List.first(remainder)
-    new_list = if ( current > next ), do: List.replace_at(remainder, 0, current), else: remainder
+    new_list = if current > next, do: List.replace_at(remainder, 0, current), else: remainder
     if (length(remainder) === 0), do: current, else: compare(new_list)
   end
 end
