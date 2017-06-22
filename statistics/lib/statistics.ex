e85ef0d2a6962bdf34dd4314f4b05245dae8bffc
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-22 11:37:13 +0200

    Refactored statistics

--- statistics/lib/statistics.ex
+++ statistics/lib/statistics.ex
@@ -1,30 +1,36 @@
 defmodule Statistics do
-  @numberList [5, 3, -42, 2, 4, 1337, 1442, 6, 2, 1, 3, 7, 100, 122] 
+  @number_list [ -1, -2, -3, -4, -5, -6 ]
 
   def initialize do
     get_max()
     get_min()
     get_mean()
     get_range()
+    get_median()
   end
 
   def get_max do
-    max_value = Max.get(@numberList)     
+    max_value = Max.get(@number_list)     
     Presentation.display("Max", max_value)
   end
 
   def get_min do
-    min_value = Min.get(@numberList)
+    min_value = Min.get(@number_list)
     Presentation.display("Min", min_value)
   end
 
   def get_mean do
-    mean_value = Mean.get(@numberList)
+    mean_value = Mean.get(@number_list)
     Presentation.display("Mean", mean_value)
   end
 
   def get_range do
-    range_value = Range.get(@numberList)
+    range_value = Range.get(@number_list)
     Presentation.display("Range", range_value)
+  end
+
+  def get_median do
+    median_value = Median.get(@number_list)
+    Presentation.display("Median", median_value)
   end
 end
