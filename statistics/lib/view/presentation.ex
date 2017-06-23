94a2a525f79d64ac76a8dbff744ee4a84091d7d7
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-23 10:31:12 +0200

    Added more methods for the Presentation module regarding error

--- statistics/lib/view/presentation.ex
+++ statistics/lib/view/presentation.ex
@@ -9,4 +9,21 @@ defmodule Presentation do
     IO.puts @line
     IO.puts "#{ message }: #{ value }"
   end
+
+  def display_error(message) do
+    IO.puts @line
+    IO.puts "#{ message }"
+  end
+
+  def data_too_short do
+    IO.puts @line
+    IO.puts "The data to analyze was too short."
+    IO.puts @line
+  end
+
+  def no_data do
+    IO.puts @line
+    IO.puts "There was no data to analyze."
+    IO.puts @line
+  end
 end
