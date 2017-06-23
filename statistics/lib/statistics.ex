2f954a29abb246ac428ad60607b74ef04ca4697e
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-23 10:30:40 +0200

    Statistics checks the inserted data and then analyses it

--- statistics/lib/statistics.ex
+++ statistics/lib/statistics.ex
@@ -5,44 +5,53 @@ defmodule Statistics do
     that are called are located in the controller, and view folder.
   """
 
-  @number_list [ 1, 2, 6, 3, 4, 5, 3, 6, 6, 2, 2 ]
-
-  def initialize do
-    get_max()
-    get_min()
-    get_mean()
-    get_range()
-    get_median()
-    get_mode()
+  def analyze(data) do
+    data_exists = !data or length(data) === 0
+    check_data(data, data_exists)
   end
 
-  def get_max do
-    max_value = Max.get(@number_list)     
+  def analyze, do: Presentation.no_data
+
+  defp check_data(data, data_exists) do
+    if !data_exists, do: initialize_analyze(data), else: Presentation.data_too_short
+  end
+
+  defp initialize_analyze(data) do
+    get_max(data)
+    get_min(data)
+    get_mean(data)
+    get_range(data)
+    get_median(data)
+    get_mode(data)
+  end
+
+  defp get_max(data) do
+    max_value = Max.get(data)     
     Presentation.display("Max", max_value)
   end
 
-  def get_min do
-    min_value = Min.get(@number_list)
+  defp get_min(data) do
+    min_value = Min.get(data)
     Presentation.display("Min", min_value)
   end
 
-  def get_mean do
-    mean_value = Mean.get(@number_list)
+  defp get_mean(data) do
+    mean_value = Mean.get(data)
     Presentation.display("Mean", mean_value)
   end
 
-  def get_range do
-    range_value = Range.get(@number_list)
+  defp get_range(data) do
+    range_value = Range.get(data)
     Presentation.display("Range", range_value)
   end
 
-  def get_median do
-    median_value = Median.get(@number_list)
+  defp get_median(data) do
+    median_value = Median.get(data)
     Presentation.display("Median", median_value)
   end
 
-  def get_mode do
-    mode_value = Mode.get(@number_list)
+  defp get_mode(data) do
+    mode_value = Mode.get(data)
     Presentation.display("Mode", mode_value)
   end
 end
