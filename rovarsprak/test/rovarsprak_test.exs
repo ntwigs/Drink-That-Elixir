b7eb9656d84a31d9d2afa46dc09c760c9a7c2608
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-24 23:15:15 +0200

    Added simple tests for converter

--- rovarsprak/test/rovarsprak_test.exs
+++ rovarsprak/test/rovarsprak_test.exs
@@ -2,7 +2,10 @@ defmodule RovarsprakTest do
   use ExUnit.Case
   doctest Rovarsprak
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Testing word conversion" do
+    assert Rovarsprak.Controller.compare_word_consonants("storm-trooper") == "sostotorormom-totroroopoperor"
+    assert Rovarsprak.Controller.compare_word_consonants("darth-vader") == "dodarortothoh-vovadoderor"
+    assert Rovarsprak.Controller.compare_word_consonants("jedi") == "jojedodi"
+    assert Rovarsprak.Controller.compare_word_consonants("yoda") == "yododa"
   end
 end
