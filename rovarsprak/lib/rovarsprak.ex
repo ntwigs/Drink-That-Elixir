4bb8593d58380185c081de9f92160cde8ddd2443
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-24 15:01:51 +0200

    added rovarsprak index

---
+++ rovarsprak/lib/rovarsprak.ex
@@ -0,0 +1,7 @@
+defmodule Rovarsprak do
+  @moduledoc """
+    This module is the start point of the application and is only used to get the show on the road
+  """
+
+  def initialize, do: Rovarsprak.Controller.convert
+end
