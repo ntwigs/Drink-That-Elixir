f4ae5da6648e7275b414ea4222d8aa996031e65f
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-24 13:57:29 +0200

    Converts to rovarsprak

--- rovarsprak/lib/rovarsprak.ex
+++ rovarsprak/lib/rovarsprak.ex
@@ -3,10 +3,35 @@ defmodule Rovarsprak do
 
   def convert do
     word_to_convert = IO.gets "Enter a word to convert: " |> String.trim()
+    IO.puts word_to_convert
     compare_word_consonants(word_to_convert)
+    want_another_conversion = String.downcase(another_conversion)
+
+    if want_another_conversion === "y\n" do
+      convert
+    end
+
+    # case want_another_conversion do
+    #   "y" -> convert
+    #   "n" -> :ok
+    #   _ -> IO.puts "Please enter y / n"
+    # end
   end
 
   def compare_word_consonants(word_to_convert) do
-    
+    letters_in_word = String.split(word_to_convert, "")
+    compare(letters_in_word)
   end
+
+  defp compare([letter|remaining], accumulator \\ "") do
+    letter_as_char = String.to_charlist(letter)
+    exists = Enum.find(@consonants, false, fn (consonant) -> consonant === letter_as_char end)
+    new_letter = if exists, do: "#{ letter }o#{ letter }", else: letter
+    combined_string = "#{ accumulator }#{ new_letter }"
+    compare(remaining, combined_string)
+  end
+
+  defp compare([], accumulator), do: IO.puts accumulator
+
+  defp another_conversion, do: IO.gets "Would you like to convert another word? (y/n)"
 end
