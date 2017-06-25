a4440f7287f807dac5b51923ff42e67850ee8e87
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-25 11:14:59 +0200

    Added result presentation in view

--- livetweets/lib/live_tweets_view.ex
+++ livetweets/lib/live_tweets_view.ex
@@ -1,3 +1,4 @@
 defmodule LiveTweets.View do
-    def keyword_query, do: IO.gets "What would you like to search for?: " |> String.strip()
+  def keyword_query, do: IO.gets "What would you like to search for?: " |> String.strip()
+  def present_result(result), do: IO.puts "#{ result }\n---------------\n"
 end
