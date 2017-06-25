4b70e64cbbeafa45d54910727350ce0038fbb14c
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-25 10:00:50 +0200

    Initialized Live Tweets

---
+++ livetweets/lib/live_tweets.ex
@@ -0,0 +1,18 @@
+defmodule LiveTweets do
+  @moduledoc """
+  Documentation for Livetweets.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Livetweets.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ livetweets/test/livetweets_test.exs
@@ -0,0 +1,8 @@
+defmodule LivetweetsTest do
+  use ExUnit.Case
+  doctest Livetweets
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
