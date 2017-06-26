3cf80a8e9f0bc89740f048de10fc6f76c4b07d78
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-06-26 12:46:58 +0200

    Added a simple test which checks the main functionallity

--- livetweets/test/livetweets_test.exs
+++ livetweets/test/livetweets_test.exs
@@ -1,8 +1,24 @@
-defmodule LivetweetsTest do
+defmodule LiveTweetsTest do
   use ExUnit.Case
-  doctest Livetweets
+  doctest LiveTweets
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Test occurance" do
+    first_list = [
+      "There's nothing you could have done, Luke, had you been there.",
+      "How long have you had these droids? About three or four seasons."
+    ]
+
+    first_result = [
+      "you - 3\n", "had - 2\n", "have - 2\n", "about - 1\n",
+      "been - 1\n", "could - 1\n", "done, - 1\n", "droids? - 1\n",
+      "four - 1\n", "how - 1\n", "long - 1\n", "luke, - 1\n",
+      "nothing - 1\n", "or - 1\n", "seasons. - 1\n", "there's - 1\n",
+      "there. - 1\n", "these - 1\n", "three - 1\n"
+    ]
+
+    assert LiveTweets.DataHandler.handle_data(first_list) === first_result
+    assert LiveTweets.DataHandler.handle_data(first_list) !== first_list
   end
+
+  
 end
